0a95ea8a21bb3fa6ddfaaac503a30f994b229885
Ujjwal-2131 <2024btechaidsujjwal17113@poornima.edu.in>
2025-08-26 23:08:47 +0530

    First commit

---
+++ code.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World " << "from Poornima University";
+
+    return 0;
+}
+// Boilerplate Code - Commentg
---
+++ code2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout <<"Ujjwal\nSinha";
+    return 0;
+}
